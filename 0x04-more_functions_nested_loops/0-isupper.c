d65b08973db99ef84bd18b04d23786fc4fb551fd
britfitzm <brittanyfitzm99@gmail.com>
2023-02-23 11:22:02 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if a letter is uppercase
+ * @c: The letter to be checked
+ * Return: 1 for uppercase letter or 0 for anything else
+ */
+
+int _isupper(int c)
+
+{
+	if (x >= 65 && x <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
