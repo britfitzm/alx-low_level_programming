3d55ce433ae388a563744e2989631214575de32d
britfitzm <brittanyfitzm99@gmail.com>
2023-03-10 05:59:08 -0800

    c files task 0

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if a letter is uppercase
+ * @c: The letter to be checked
+ * Return: 1 for uppercase letter or 0 for anything else
+ */
+
+int _isupper(int c)
+
+{
+	if (c >= 65 && c <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
