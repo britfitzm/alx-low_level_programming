7704925f7a7a1957a103c4be226041918d2af0ce
britfitzm <brittanyfitzm99@gmail.com>
2023-03-10 11:42:25 -0800

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: Always 0 (Succes)
+ */
+
+int main(int argc__attribute__((unused)), char *argv[])
+
+{
+	printf("%s\n", *argv);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This directory contains all the answers to the tasks of project 0x0A.C-argc,argv
