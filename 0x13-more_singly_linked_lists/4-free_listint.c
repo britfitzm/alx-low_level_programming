3d11ef74e9c01e0af118a6c4cf5a7c2b7ebeddad
britfitzm <brittanyfitzm99@gmail.com>
2023-04-03 10:19:54 -0700

    4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint - frees list
+ * @head: head of linked list
+ * Return: no return
+ */
+
+void free_listint(listint_t *head)
+
+{
+	listint_t *current_node;
+
+	while ((current_node = head) != NULL)
+	{
+		head = head->next;
+		free(current_node);
+	}
+}
